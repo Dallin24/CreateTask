e3d7a4b94bb93795a0bb9a6c8b6f0459f4d6158a
Dallin24 <the4gates24@gmail.com>
2021-10-08 12:37:38 -0600

    CreateTask Base Project

---
+++ CreateTask/Resources/CreateTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreateTaskApp.swift
+//  CreateTask
+//
+//  Created by Gibbs, Dallin on 10/8/21.
+//
+
+import SwiftUI
+
+@main
+struct CreateTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
