ede79e644299c3e8a4b44deedf25324b6aabf6a6
Dallin24 <the4gates24@gmail.com>
2021-10-18 13:19:17 -0600

    Transformed Use of ForEach Loop, Deleted DataLines Data Type

--- CreateTask/BarChartView.swift
+++ CreateTask/BarChartView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct BarChartView: View
 {
+	@State public var barLengths: [Int] = [0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40,42,44,46,48]
 	
 	var body: some View
 	{
@@ -16,35 +17,28 @@ struct BarChartView: View
 		{
 			VStack(alignment: .leading, spacing: 1.0)
 			{
-				ForEach(barData)
+				ForEach(0..<barTimes.count)
 				{
-					data in
-					Button(data.time, action: addCount).padding(0.0).frame(width: 50.0).background(Color(.systemGray3)).foregroundColor(.black).cornerRadius(5)
+					timeSlot in
+					Button(action:{ changeLength(number: timeSlot) }, label: { Text("\(barTimes[timeSlot])") })
+						.padding(0.0).frame(width: 50.0).background(Color(.systemGray3)).foregroundColor(.black).cornerRadius(5)
+					
 				}
 			}
-			
+			/*
 			VStack(alignment: .leading, spacing: 1.0)
 			{
-				ForEach(barData)
+				ForEach(barLengths, id: \.self)
 				{
-					data in
-					Text(data.length).background(Color(.blue))
+					length in
+					Text(length).background(Color(.blue))
 				}
-			}
+			}*/
 		}
-		
     }
 	
-	func addCount() -> Void
+	func changeLength(number: Int) -> Void
 	{
-		var dataPoints : [String] = []
-		
-		for data in barData
-		{
-			
-			dataPoints.append(data.length)
-		}
-		
 		
 	}
 }
