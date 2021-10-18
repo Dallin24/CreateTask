5e3c451591b966b9ebf5a75fdcc1842dca3bc7bd
Dallin24 <the4gates24@gmail.com>
2021-10-18 13:20:27 -0600

    Adjusted Spacing For Readability

--- CreateTask/BarChartView.swift
+++ CreateTask/BarChartView.swift
@@ -10,7 +10,6 @@ import SwiftUI
 struct BarChartView: View
 {
 	@State public var barLengths: [Int] = [0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40,42,44,46,48]
-	
 	var body: some View
 	{
 		HStack
@@ -20,8 +19,14 @@ struct BarChartView: View
 				ForEach(0..<barTimes.count)
 				{
 					timeSlot in
-					Button(action:{ changeLength(number: timeSlot) }, label: { Text("\(barTimes[timeSlot])") })
-						.padding(0.0).frame(width: 50.0).background(Color(.systemGray3)).foregroundColor(.black).cornerRadius(5)
+					Button(action:
+					{
+						changeLength(number: timeSlot)
+					}, label:
+					{
+						Text("\(barTimes[timeSlot])")
+					})
+					.padding(0.0).frame(width: 50.0).background(Color(.systemGray3)).foregroundColor(.black).cornerRadius(5)
 					
 				}
 			}
