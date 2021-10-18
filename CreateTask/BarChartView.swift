ae8afdd01c6226ca805479442284079b1259eec2
Dallin24 <the4gates24@gmail.com>
2021-10-18 13:25:43 -0600

    Condensed Code Into Less Files

--- CreateTask/BarChartView.swift
+++ CreateTask/BarChartView.swift
@@ -9,7 +9,65 @@ import SwiftUI
 
 struct BarChartView: View
 {
-	@State public var barLengths: [Int] = [0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40,42,44,46,48]
+	@State public var barLengths: [Int] =
+		[
+			0,
+			2,
+			4,
+			6,
+			8,
+			10,
+			12,
+			14,
+			16,
+			18,
+			20,
+			22,
+			24,
+			26,
+			28,
+			30,
+			32,
+			34,
+			36,
+			38,
+			40,
+			42,
+			44,
+			46,
+			48
+		]
+	
+	let barTimes: [String] =
+		[
+			"12AM",
+			"1AM",
+			"2AM",
+			"3AM",
+			"4AM",
+			"5AM",
+			
+			"6AM",
+			"7AM",
+			"8AM",
+			"9AM",
+			"10AM",
+			"11AM",
+			
+			"12PM",
+			"1PM",
+			"2PM",
+			"3PM",
+			"4PM",
+			"5PM",
+			
+			"6PM",
+			"7PM",
+			"8PM",
+			"9PM",
+			"10PM",
+			"11PM",
+		]
 	var body: some View
 	{
 		HStack
@@ -30,19 +88,20 @@ struct BarChartView: View
 					
 				}
 			}
-			/*
-			VStack(alignment: .leading, spacing: 1.0)
+			
+			/*VStack(alignment: .leading, spacing: 1.0)
 			{
 				ForEach(barLengths, id: \.self)
 				{
 					length in
-					Text(length).background(Color(.blue))
+					Text(length)
+					//.background(Color(.blue))
 				}
 			}*/
 		}
     }
 	
-	func changeLength(number: Int) -> Void
+	func changeLength(number: Int)
 	{
 		
 	}
