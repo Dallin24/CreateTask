040bd87625ea5d9d9e03ac367e111af80494f0f8
Dallin24 <the4gates24@gmail.com>
2021-11-09 13:10:17 -0700

    Created SortingAlgorithimsView

---
+++ CreateTask/ViewController/SortingAlgorithimsView.swift
@@ -0,0 +1,23 @@
+//
+//  SortingAlgorithimsView.swift
+//  CreateTask
+//
+//  Created by Gibbs, Dallin on 11/9/21.
+//
+
+import SwiftUI
+
+struct SortingAlgorithimsView: View {
+	
+	
+	
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SortingAlgorithimsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SortingAlgorithimsView()
+    }
+}
